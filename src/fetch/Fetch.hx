2b22e3fc7b619466915ebf79e5f0da206adad55a
Cédric Belin <cedric@belin.io>
2020-06-08 20:45:32 +0200

    Added externs for the "node-fetch" package

---
+++ src/fetch/Fetch.hx
@@ -0,0 +1,18 @@
+package fetch;
+
+#if nodejs
+import haxe.extern.EitherType;
+import js.html.Request;
+import js.html.RequestInit;
+import js.html.Response;
+import js.lib.Promise;
+
+/** Fetches resources from the network. **/
+@:jsRequire("node-fetch")
+extern class Fetch {
+
+	/** Fetches the resource corresponding to the specified `request`. **/
+	@:selfCall
+	public static function fetch(request: EitherType<String, Request>, ?options: RequestInit): Promise<Response>;
+}
+#end
