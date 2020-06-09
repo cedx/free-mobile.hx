b0b13c53268df9d43ada398cc9b91d2c06c71ad9
Cédric Belin <cedric@belin.io>
2020-06-09 02:04:56 +0200

    Added the generated PHP sources

--- src/free_mobile/Client.hx
+++ src/free_mobile/Client.hx
@@ -1,12 +1,12 @@
 package free_mobile;
 
-import haxe.Exception;
 import haxe.io.Path;
 import thenshim.Promise;
 
 using StringTools;
 
 #if nodejs
+import haxe.Exception;
 import fetch.Fetch;
 import js.html.URLSearchParams;
 #else
@@ -39,7 +39,7 @@ import haxe.Http;
 	public dynamic function onResponse(event: RequestEvent) {}
 
 	/** Sends a SMS message to the underlying account. **/
-	public function sendMessage(text: String): Promise<Void> {
+	public function sendMessage(text: String): Promise<Any> {
 		final message = text.trim().substring(0, 160);
 		final url = '$endPoint/sendmsg';
 
@@ -50,10 +50,10 @@ import haxe.Http;
 			query.append("user", username);
 
 			onRequest(new RequestEvent("request", this, url));
-			return new Fetch('$url?$query')
+			return Fetch.fetch('$url?$query')
 				.then(response -> {
 					onResponse(new RequestEvent("response", this, url));
-					response.ok ? null : throw new ClientException("An error occurred while querying the end point.", url);
+					response.ok ? null : throw new Exception("An error occurred while querying the end point.", url);
 				})
 				.catchError(error -> throw new ClientException(Std.string(error), url));
 		#else
@@ -62,8 +62,8 @@ import haxe.Http;
 			http.addParameter("pass", password);
 			http.addParameter("user", username);
 
-			onRequest(new RequestEvent("request", this, url));
 			return new Promise((resolve, reject) -> {
+				onRequest(new RequestEvent("request", this, url));
 				http.onBytes = bytes -> { onResponse(new RequestEvent("response", this, url)); resolve(null); };
 				http.onError = error -> reject(new ClientException(error, http.url));
 				http.request(false);
--- test/free_mobile/ClientTest.hx
+++ test/free_mobile/ClientTest.hx
@@ -9,12 +9,6 @@ using thenshim.PromiseTools;
 /** Tests the features of the `Client` class. **/
 class ClientTest extends Test {
 
-	/** Tests the constructor. **/
-	function testConstructor() {
-		// It should throw an exception if the credentials are invalid.
-		Assert.raises(() -> new Client("", ""));
-	}
-
 	/** Tests the `sendMessage()` method. **/
 	function testSendMessage(async: Async) {
 		// It should reject if a network error occurred.
@@ -29,16 +23,15 @@ class ClientTest extends Test {
 				branch.done();
 			}));
 
-		// It should trigger events.
+		// It should emit events.
 		client = new Client(Sys.getEnv("FREEMOBILE_USERNAME"), Sys.getEnv("FREEMOBILE_PASSWORD"));
-		async.branch(branch -> client.onRequest = function(event) {
+		client.onRequest = event -> {
 			Assert.equals(client, event.client);
 			Assert.equals("request", event.name);
 			Assert.equals('${client.endPoint}/sendmsg', event.url);
-			branch.done();
-		});
+		};
 
-		// It should send SMS messages if credentials are valid.
+		// It should resolve if the message is sent.
 		async.branch(branch -> client.sendMessage("Bonjour Cédric, à partir de Haxe !")
 			.then(_ -> {
 				Assert.pass();
