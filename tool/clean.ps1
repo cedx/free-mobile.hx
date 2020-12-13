23e69856241dd24e9bef0f79de1b37c22928654a
Cédric Belin <cedric@belin.io>
2020-12-13 19:07:05 +0100

    Updated the build system

--- tool/clean.ps1
+++ tool/clean.ps1
@@ -2,8 +2,9 @@
 Set-StrictMode -Version Latest
 Set-Location (Split-Path $PSScriptRoot)
 
-foreach ($item in "docs/api", "lib/index.php", "lib/js/bundle.*", "lib/php", "lib/res") {
+foreach ($item in "docs/api", "lib/index.php", "lib/js/bundle.*", "lib/res") {
 	if (Test-Path $item) { Remove-Item $item -Force -Recurse }
 }
 
+Remove-Item lib/php/* -Exclude adapter.php -Force -Recurse
 Remove-Item var/* -Exclude .gitkeep -Force -Recurse
--- tool/dist.ps1
+++ tool/dist.ps1
@@ -6,4 +6,5 @@ tool/clean.ps1
 tool/version.ps1
 tool/build.ps1
 
-Get-ChildItem lib/php -Exclude free_mobile | Remove-Item -Force -Recurse
+Remove-Item lib/index.php
+Get-ChildItem lib/php -Exclude adapter.php, free_mobile | Remove-Item -Force -Recurse
