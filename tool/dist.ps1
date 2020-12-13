91ca0c39c2e4e313a5c338637b4377ed62c94934
Cédric Belin <cedric@belin.io>
2020-12-13 21:08:55 +0100

    Updated the PHP dependencies

--- tool/dist.ps1
+++ tool/dist.ps1
@@ -6,4 +6,6 @@ tool/clean.ps1
 tool/version.ps1
 tool/build.ps1
 
-Get-ChildItem lib/php -Exclude adapter.php, free_mobile | Remove-Item -Force -Recurse
+Get-ChildItem lib/php -Exclude adapter.php, free_mobile, tink | Remove-Item -Force -Recurse
+Get-ChildItem lib/php/tink -Exclude querystring | Remove-Item -Force -Recurse
+Get-ChildItem lib/php/tink/querystring -Exclude *0.php | Remove-Item -Force -Recurse
