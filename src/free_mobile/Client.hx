c9109689af461208b58aae0b772197d8cf983f75
Cédric Belin <cedric@belin.io>
2021-10-11 20:12:38 +0200

    Updated the HTTP client

--- src/free_mobile/Client.hx
+++ src/free_mobile/Client.hx
@@ -1,43 +1,53 @@
 package free_mobile;
 
 import tink.QueryString;
-import tink.http.Client as Http;
+import tink.Url;
+import tink.http.Client as HttpClient;
 
 using StringTools;
 using haxe.io.Path;
 
 /** Sends messages by SMS to a [Free Mobile](https://mobile.free.fr) account. **/
-@:expose class Client {
+class Client {
 
-	/** The URL of the API end point. **/
-	final endPoint: String;
+	/** The Free Mobile account. **/
+	public final account: String;
 
-	/** The identification key associated to the account. **/
-	final password: String;
+	/** The Free Mobile API key. **/
+	public final apiKey: String;
 
-	/** The user name associated to the account. **/
-	final username: String;
+	/** The base URL of the remote API endpoint. **/
+	public final baseUrl: Url = "https://smsapi.free-mobile.fr/";
 
 	/** Creates a new client. **/
-	public function new(username: String, password: String, endPoint = "https://smsapi.free-mobile.fr") {
-		this.endPoint = endPoint.removeTrailingSlashes();
-		this.password = password;
-		this.username = username;
+	public function new(account: String, apiKey: String, ?baseUrl: Url) {
+		this.account = account;
+		this.apiKey = apiKey;
+		if (baseUrl != null) this.baseUrl = Path.addTrailingSlash(baseUrl);
 	}
 
 	/** Sends a SMS message to the underlying account. **/
 	public function sendMessage(text: String): Promise<Noise> {
-		final url = '$endPoint/sendmsg?' + QueryString.build({
+		final queryString = QueryString.build({
 			msg: text.trim().substring(0, 160),
-			pass: password,
-			user: username
+			pass: apiKey,
+			user: account
 		});
 
-		return try Http.fetch(url).map(outcome -> switch outcome {
-			case Success(_): Success(Noise);
-			case Failure(error): error.code == InternalError && ~/content-length header is required/i.match(error.message)
-				? Success(Noise)
-				: Failure(error);
-		}) catch (e) new Error(e.message);
+		return HttpClient.fetch(baseUrl.resolve('sendmsg?$queryString')).noise();
+
+		/* TODO
+		.map(outcome -> {
+			trace(outcome);
+			switch outcome {
+				case Success(_): Success(Noise);
+				case Failure(error): error.code == InternalError && ~/content-length header is required/i.match(error.message)
+						? Success(Noise)
+						: Failure(error);
+			}
+		}) catch (e) {
+			trace(e);
+			new Error(e.message);
+		}*/
 	}
 }
---
+++ src/free_mobile/Version.hx
@@ -0,0 +1,38 @@
+package free_mobile;
+
+#if macro
+import haxe.Json;
+import haxe.macro.Context;
+import sys.io.File;
+import sys.io.Process;
+#end
+
+/** Provides information about the program version. **/
+@:noDoc abstract class Version {
+
+	/** Gets the hash of the current Git commit. **/
+	macro public static function getGitCommitHash() {
+		#if display
+			return macro $v{""};
+		#else
+			final process = new Process("git", ["rev-parse", "HEAD"]);
+			final hash = process.exitCode() == 0 ? process.stdout.readLine() : process.stderr.readLine();
+			process.close();
+			return macro $v{hash};
+		#end
+	}
+
+	/** Gets the name of the Haxe target. **/
+	macro public static function getHaxeTarget() return macro $v{Context.definedValue("target.name")};
+
+	/** Gets the version of the Haxe compiler. **/
+	macro public static function getHaxeVersion() return macro $v{Context.definedValue("haxe")};
+
+	/** Gets the package version of this program. **/
+	macro public static function getPackageVersion()
+		#if display
+			return macro $v{"0.0.0"};
+		#else
+			return macro $v{Json.parse(File.getContent("haxelib.json")).version};
+		#end
+}
