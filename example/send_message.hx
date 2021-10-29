0f3d0ecb1d52261a215b24fba13c35df3ca25616
Cédric Belin <cedric@belin.io>
2021-10-29 23:40:30 +0200

    Updated the example code

---
+++ example/send_message.hx
@@ -0,0 +1,13 @@
+import free_mobile.Client;
+using tink.CoreApi;
+
+/** Sends an SMS notification. **/
+function main() {
+	final client = new Client("your account identifier", "your API key");
+	// For example: new Client("12345678", "a9BkVohJun4MAf")
+
+	client.sendMessage("Hello World from Haxe!").handle(outcome -> switch outcome {
+		case Success(_): trace("The message was sent successfully.");
+		case Failure(error): trace('An error occurred: ${error.message}');
+	});
+}
