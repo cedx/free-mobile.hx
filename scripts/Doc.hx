cce069031eae1f2f1a61361ba67adbdf11d65192
Cédric Belin <cedric@belin.io>
2021-11-11 22:18:42 +0100

    Updated the documentation

--- scripts/Doc.hx
+++ scripts/Doc.hx
@@ -6,21 +6,21 @@ import sys.io.File.*;
 
 /** Runs the script. **/
 function main() {
-	if (exists("docs/api")) removeDirectory("docs/api");
+	if (exists("docs")) removeDirectory("docs");
 
 	command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
 	command("lix", [
 		"run", "dox",
 		"--define", "description", "Send SMS messages to your Free Mobile device, in Haxe.",
-		"--define", "source-path", "https://github.com/cedx/free-mobile.hx/blob/main/src",
+		"--define", "source-path", "https://bitbucket.org/cedx/free-mobile.hx/src/main/src",
 		"--define", "themeColor", "0xffc105",
 		"--define", "version", Json.parse(getContent("haxelib.json")).version,
-		"--define", "website", "https://cedx.github.io/free-mobile.hx",
+		"--define", "website", "https://bitbucket.org/cedx/free-mobile.hx",
 		"--input-path", "var",
-		"--output-path", "docs/api",
+		"--output-path", "docs",
 		"--title", "Free Mobile for Haxe",
 		"--toplevel-package", "free_mobile"
 	]);
 
-	copy("docs/favicon.ico", "docs/api/favicon.ico");
+	copy("www/favicon.ico", "docs/favicon.ico");
 }
