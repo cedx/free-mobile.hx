8377dacc7f375bb858d6b006c80e5ff3d353754e
Cédric Belin <cedric@belin.io>
2023-02-06 00:21:58 +0100

    Ported the build system to Lix scripts

---
+++ scripts/Doc.hx
@@ -0,0 +1,25 @@
+//! --class-path src --library tink_core
+import free_mobile.Version;
+import sys.FileSystem;
+import sys.io.File;
+
+/** Runs the script. **/
+function main() {
+	if (FileSystem.exists("docs")) Tools.removeDirectory("docs");
+
+	Sys.command("haxe --define doc-gen --no-output --xml var/api.xml build.hxml");
+	Sys.command("lix", [
+		"run", "dox",
+		"--define", "description", "Send SMS messages to your Free Mobile device, in Haxe.",
+		"--define", "source-path", "https://github.com/cedx/free-mobile.hx/blob/main/src",
+		"--define", "themeColor", "0xffc105",
+		"--define", "version", Version.packageVersion,
+		"--define", "website", "https://github.com/cedx/free-mobile.hx",
+		"--input-path", "var",
+		"--output-path", "docs",
+		"--title", "Free Mobile for Haxe",
+		"--toplevel-package", "free_mobile"
+	]);
+
+	File.copy("www/favicon.ico", "docs/favicon.ico");
+}
