5ea7c7adee32a2bfe8b912ca325c5c6dc6828edf
Cédric Belin <cedric@belin.io>
2023-05-12 16:21:11 +0200

    Update the dependencies [skip ci]

--- src/free_mobile/RemoteApi.hx
+++ src/free_mobile/RemoteApi.hx
@@ -1,7 +1,7 @@
 package free_mobile;
 
 /** Defines the interface of the remote API. **/
-interface RemoteApi {
+@:noDoc interface RemoteApi {
 
 	/** Sends a SMS message to the specified Free Mobile account. **/
 	@:get function sendmsg(query: {user: String, pass: String, msg: String}): Noise;
