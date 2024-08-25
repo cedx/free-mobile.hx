5343d1e0993935bfaa8fbfcb961478f30e1046c7
Cédric Belin <cedric@belin.io>
2024-08-26 01:38:51 +0200

    Remove unused metadata

--- src/free_mobile/Client.hx
+++ src/free_mobile/Client.hx
@@ -30,9 +30,6 @@ final class Client {
 	}
 
 	/** Sends a SMS message to the underlying account. **/
-	public function sendMessage(text: String) return remote.sendmsg({
-		user: account,
-		pass: apiKey,
-		msg: text.trim().substr(0, 160)
-	});
+	public function sendMessage(text: String): Promise<Noise>
+		return remote.sendmsg({user: account, pass: apiKey, msg: text.trim().substr(0, 160)});
 }
--- src/free_mobile/RemoteApi.hx
+++ src/free_mobile/RemoteApi.hx
@@ -1,7 +1,7 @@
 package free_mobile;
 
 /** Defines the interface of the remote API. **/
-@:noDoc interface RemoteApi {
+interface RemoteApi {
 
 	/** Sends a SMS message to the specified Free Mobile account. **/
 	@:get function sendmsg(query: {user: String, pass: String, msg: String}): Noise;
