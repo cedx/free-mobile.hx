d965d02d0e6d15c0afeeb390e29a7c0b18f60128
Cédric Belin <cedric@belin.io>
2024-10-19 13:17:41 +0200

    Update the "Clean" task [skip ci]

--- scripts/Clean.hx
+++ scripts/Clean.hx
@@ -9,13 +9,13 @@ function main() {
 }
 
 /** Recursively deletes all files in the specified `directory`. **/
-function cleanDirectory(directory: String) for (entry in FileSystem.readDirectory(directory).filter(entry -> entry != ".gitkeep")) {
+private function cleanDirectory(directory: String) for (entry in FileSystem.readDirectory(directory).filter(entry -> entry != ".gitkeep")) {
 	final path = Path.join([directory, entry]);
 	FileSystem.isDirectory(path) ? removeDirectory(path) : FileSystem.deleteFile(path);
 }
 
 /** Recursively deletes the specified `directory`. **/
-function removeDirectory(directory: String) {
+private function removeDirectory(directory: String) {
 	cleanDirectory(directory);
 	FileSystem.deleteDirectory(directory);
 }
